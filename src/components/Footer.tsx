646d8a8727ae3de743a4d8b6a567f38cca693a08
sean roberts <seanroberts66@gmail.com>
2025-03-05 22:58:04 -0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="border-t border-odi-gray-300 pt-8">
           <div className="text-center text-body-s text-odi-gray-500">
+            <p className="mt-2">This is not an official government website or official source of information</p>
             <p className="mt-2">Cali DOGE is a project of <a href="https://opendatainitiative.io" className="text-odi-blue hover:underline" target="_blank" rel="noopener noreferrer">Open Data Initative</a> under CC-BY license.</p>
           </div>
         </div>
